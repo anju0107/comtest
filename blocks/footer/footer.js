9e888cf05d7f105d58a9fa73f02105e7a8a5356f
SiddharthMaheshwari91 <s.j.maheshwari@accenture.com>
2023-10-20 15:31:55 +1100

    Integrated Ai chatbot

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -22,4 +22,13 @@ export default async function decorate(block) {
     decorateIcons(footer);
     block.append(footer);
   }
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"1482105436ad330999fedb7b66ea215b8","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+
 }
