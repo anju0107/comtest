c0b9fa761aa28c96f591ee9fefe7b73362d1ef1e
SiddharthMaheshwari91 <s.j.maheshwari@accenture.com>
2023-10-20 23:16:25 +1100

    Updated quotes in footer js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -24,15 +24,15 @@ export default async function decorate(block) {
   }
   (function(d, komm) {
     const kommunicateSettings = {
-      "appId": "1482105436ad330999fedb7b66ea215b8",
-      "popupWidget": true,
-      "automaticChatOpenOnNavigation": true
+      'appId': '1482105436ad330999fedb7b66ea215b8',
+      'popupWidget': true,
+      'automaticChatOpenOnNavigation': true
     };
-    const script = document.createElement("script");
-    script.type = "text/javascript";
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
     script.async = true;
-    script.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-    const head = document.getElementsByTagName("head")[0];
+    script.src = 'https://widget.kommunicate.io/v2/kommunicate.app';
+    const head = document.getElementsByTagName('head')[0];
     head.appendChild(script);
     window.kommunicate = komm;
     komm._globals = kommunicateSettings;
