b2cff87d8950c2c2773ad0688b760c8fe6d38f3a
thejasmine <jasmine.the@accenture.com>
2023-10-23 11:08:54 +1100

    fix nav

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -54,11 +54,6 @@ function toggleAllNavSections(sections, expanded = false) {
  */
 function toggleMenu(nav, navSections, forceExpanded = null) {
   const expanded = forceExpanded !== null ? !forceExpanded : nav.getAttribute('aria-expanded') === 'true';
-  if (!expanded) {
-    nav.classList.add('nav-open');
-  } else {
-    nav.classList.remove('nav-open');
-  }
   const button = nav.querySelector('.nav-hamburger button');
   document.body.style.overflowY = (expanded || isDesktop.matches) ? '' : 'hidden';
   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
